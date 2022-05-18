96a8b65c3cea5aefd519a5e597b4737681515ef6
nigel.andam <nigel.andam@adec-innovations.com>
2022-05-18 11:26:01 +0800

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 text-gray-900'>Hello, welcome to my blog</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie justo sit amet urna vestibulum, 
+            a tempus tortor sodales. Nam quis dui sed orci vulputate tempus eu sit amet nunc. Mauris mi sem, scelerisque 
+            sed malesuada ut, consectetur quis arcu. Pellentesque aliquet, nibh a pharetra fermentum, justo libero cursus 
+            leo, eget malesuada lacus turpis gravida orci.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie justo sit amet urna vestibulum, 
+            a tempus tortor sodales. Nam quis dui sed orci vulputate tempus eu sit amet nunc. Mauris mi sem, scelerisque 
+            sed malesuada ut, consectetur quis arcu. Pellentesque aliquet, nibh a pharetra fermentum, justo libero cursus 
+            leo, eget malesuada lacus turpis gravida orci.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie justo sit amet urna vestibulum, 
+            a tempus tortor sodales. Nam quis dui sed orci vulputate tempus eu sit amet nunc. Mauris mi sem, scelerisque 
+            sed malesuada ut, consectetur quis arcu. Pellentesque aliquet, nibh a pharetra fermentum, justo libero cursus 
+            leo, eget malesuada lacus turpis gravida orci. Nam quis dui sed orci vulputate tempus eu sit amet nunc. 
+            Mauris mi sem, scelerisque sed malesuada ut, consectetur quis arcu.
+        </p>
+    </div>
+  )
+}
---
+++ src/postcss.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    plugins: [required('tailwindcss'), require("autoprefixer")],
+};
