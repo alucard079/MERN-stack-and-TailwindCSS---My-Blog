bf94488f15eccb65669c64e41a918a5fa91cf751
nigel.andam <nigel.andam@adec-innovations.com>
2022-05-18 13:10:25 +0800

    Update: Added pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Home = () => {
+export const About = () => {
   return (
     <div className='mb-20'>
-        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>Hello, welcome to my blog</h1>
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>About</h1>
         <p className='mx-auto leading-relaxed text-base mb-4'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie justo sit amet urna vestibulum, 
             a tempus tortor sodales. Nam quis dui sed orci vulputate tempus eu sit amet nunc. Mauris mi sem, scelerisque 
@@ -27,4 +27,4 @@ export const Home = () => {
   )
 }
 
-export default Home
+export default About;
