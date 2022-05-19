911daca252e0829c8e6993da14e878423af1913f
nigel.andam <nigel.andam@adec-innovations.com>
2022-05-19 17:09:20 +0800

    display article

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import ArticleList from './pages/ArticleList';
 import Article from './pages/Article';
+import NavBar from './components/NavBar';
+
+//Components
+
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar/>
       <div className="max-w-screen-md mx-auto pt-20" >
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
             <Route path="/articles" element={<ArticleList/>} />
-            <Route path="/article" element={<Article/>} />
+            <Route path="/article/:name" element={<Article/>} />
           </Routes>
       </div>
     </BrowserRouter>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import articleContent from './article-content';
 
-const Article = () => {
+const Article = (props) => {
+  let { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+  if(!article) {
+    return (<h1>Article does not exists</h1>)
+  }
   return (
-    <div>
-        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>This is an Article</h1>
-    </div>
+    <>
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>
+          {article.title}
+        </h1>
+        { 
+          article.content.map((paragraph, index) => (
+            <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+              {paragraph}
+            </p>
+          ))
+        }
+    </>
   )
 }
 
